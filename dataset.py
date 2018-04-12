2f012a55063b9922fe1f68e414650a2eae34f9d4
F_Tag <25476669+F-Tag@users.noreply.github.com>
2018-04-12 18:38:08 +0900

    fix

--- dataset.py
+++ dataset.py
@@ -2,18 +2,19 @@ import json
 import os
 from copy import deepcopy
 from glob import glob
-from random import choice, shuffle
+# from random import choice, shuffle
+from random import shuffle
 
-import chainer
+# import chainer
 import numpy as np
 from chainer.dataset import DatasetMixin
-from chainer.dataset.convert import to_device
-from librosa import load
-from librosa.feature import melspectrogram
-from librosa.util import frame
+# from chainer.dataset.convert import to_device
+# from librosa import load
+# from librosa.feature import melspectrogram
+# from librosa.util import frame
 from nnmnkwii.preprocessing import mulaw_quantize
-from pyvad import trim
-from scipy import signal
+# from pyvad import trim
+# from scipy import signal
 
 from .default_settings import DEFAULT
 
@@ -23,7 +24,7 @@ default_dataset_root = os.path.join(DEFAULT['datasetroot'], DEFAULT['npzdir'])
 
 class NPZDataset(DatasetMixin):
 
-    def __init__(self, dataset_root=default_dataset_root, dataset_dir="", param_file="datasetparam.json", length=7680, spec_mode='conv', mode='mixture', log_spec=True, spec_thr=1.0e-10, keydict=dict(wave='wave', lc='mspec')):
+    def __init__(self, dataset_root=default_dataset_root, dataset_dir="", param_file="datasetparam.json"):
         if dataset_dir:
             data_dir = os.path.join(dataset_root, dataset_dir)
         else:
@@ -33,89 +34,19 @@ class NPZDataset(DatasetMixin):
         self._paths = paths
         with open(os.path.join(dataset_root, param_file), 'r') as f:
             load = json.load(f)
-        
-        mspec_max = load['mspec_max']
-        pspec_max = load['pspec_max']
-        self.spec_thr = spec_thr
-        if spec_thr:
-            mspec_min = spec_thr
-            pspec_min = spec_thr
-        else:
-            mspec_min = load['mspec_min']
-            pspec_min = load['pspec_min']
-        if log_spec:
-            self.m_shift = np.log10(mspec_min)
-            self.p_shift = np.log10(pspec_min)
-            self.m_scale = np.log10(mspec_max) - np.log10(mspec_min)
-            self.p_scale = np.log10(pspec_max) - np.log10(pspec_min)
-        else:
-            self.m_shift = mspec_min
-            self.m_scale = mspec_max - mspec_min
-            self.p_shift = pspec_min
-            self.p_scale = pspec_max - pspec_min
+
+        self.mspec_max = load['mspec_max']
+        self.pspec_max = load['pspec_max']
+        self.mspec_min = load['mspec_min']
+        self.pspec_min = load['pspec_min']
         self.upsample = load['upsample']
-        self.length = length
-        self.mode = mode
-        self.spec_mode = spec_mode
-        self.keydict = keydict
-        self.log_spec = log_spec
-        
 
     def __len__(self):
         return len(self._paths)
 
     def get_example(self, i):
         path = self._paths[i]
-        return self.npzprocess(path)
-
-    def npzprocess(self, path):
-        load = dict(np.load(path))
-        if self.length:
-            if len(load['wave']) <= self.length:
-                load['wave'] = np.pad(
-                    load['wave'], (0, self.length - len(load['wave']) + 1), 'constant', constant_values=0)
-                spec_len = self.length // self.upsample
-                load['mspec'] = np.pad(
-                    load['mspec'], (0, spec_len - len(load['mspec'])), 'constant', constant_values=self.m_shift)
-                load['pspec'] = np.pad(
-                    load['pspec'], (0, spec_len - len(load['pspec'])), 'constant', constant_values=self.p_shift)
-                index = 0
-            else:
-                index = np.random.randint(0, len(load['wave']) - self.length)
-
-            index = (index // self.upsample) * self.upsample
-            load['wave'] = load['wave'][index:index + self.length + 1]
-            load['mspec'] = load['mspec'][index //
-                                          self.upsample:(index + self.length) // self.upsample]
-            load['pspec'] = load['pspec'][index //
-                                          self.upsample:(index + self.length) // self.upsample]
-        
-        if self.spec_thr:
-            load['pspec'] = np.clip(load['pspec'], self.spec_thr, None)
-            load['mspec'] = np.clip(load['mspec'], self.spec_thr, None)
-
-        if self.log_spec:
-            load['pspec'] = np.log10(load['pspec'])
-            load['mspec'] = np.log10(load['mspec'])
-
-        rdict = {}
-        for rkey, key in self.keydict.items():
-            if key == 'mspec':
-                rdict[rkey] = ((load.pop('mspec') - self.m_shift) /
-                               self.m_scale).astype('float32')
-                if self.spec_mode == 'conv':
-                    rdict[rkey] = rdict[rkey].T
-            elif key == 'pspec':
-                rdict[rkey] = ((load.pop('pspec') - self.p_shift) /
-                               self.p_scale).astype('float32')
-                if self.spec_mode == 'conv':
-                    rdict[rkey] = rdict[rkey].T
-            elif key == 'wave':
-                rdict[rkey] = (load.pop('wave').reshape(
-                    1, -1) / (2.0**15 - 1)).astype('float32')
-                if self.mode == 'softmax':
-                    rdict[rkey] = mulaw_quantize(rdict[rkey]).astype('int32')
-        return rdict
+        return dict(np.load(path))
 
     def get_example_from_names(self, names, random=True):
         names = deepcopy(names)
@@ -135,9 +66,84 @@ class NPZDataset(DatasetMixin):
 
         if path is None:
             raise FileNotFoundError
-            return None
 
-        return self.npzprocess(path)
+        return dict(np.load(path))
+
+
+def transform_for_wnv(indata, spec_max, spec_min=1e-10, length=7680, upsample=80, log_spec=True, softmax=False):
+    if length:
+        if len(indata['wave']) <= length:
+            indata['wave'] = np.pad(
+                indata['wave'], (0, length - len(indata['wave']) + 1), 'constant', constant_values=0)
+            spec_len = length // upsample
+            indata['mspec'] = np.pad(
+                indata['mspec'], (0, spec_len - len(indata['mspec'])), 'constant', constant_values=spec_min)
+            index = 0
+        else:
+            index = np.random.randint(0, len(indata['wave']) - length)
+
+        index = (index // upsample) * upsample
+        indata['wave'] = indata['wave'][index:index + length + 1]
+        indata['mspec'] = indata['mspec'][index //
+                                          upsample:(index + length) // upsample]
+
+    indata['mspec'] = np.clip(indata['mspec'], spec_min, spec_max)
+    if log_spec:
+        indata['mspec'] = np.log10(indata['mspec'])
+        shift = np.log10(spec_min)
+        scale = np.log10(spec_max) - np.log10(spec_min)
+        indata['mspec'] = (indata['mspec'] - shift) / scale
+    else:
+        shift = spec_min
+        scale = spec_max - spec_min
+        indata['mspec'] = (indata['mspec'] - shift) / scale
+
+    rdict = {}
+    rdict['lc'] = indata['mspec'].astype('float32').T
+    rdict['wave'] = (indata('wave').reshape(
+        1, -1) / (2.0**15 - 1)).astype('float32')
+    if softmax:
+        rdict['wave'] = mulaw_quantize(rdict['wave']).astype('int32')
+    return rdict
+
+
+def transform_for_ssrn(indata, mspec_max, pspec_max, length=None, mspec_min=1e-10, pspec_min=1e-10, log_spec=True):
+    if length:
+        if len(indata['mspec']) <= length:
+            indata['mspec'] = np.pad(
+                indata['mspec'], (0, length - len(indata['mspec'])), 'constant', constant_values=mspec_min)
+            indata['pspec'] = np.pad(
+                indata['pspec'], (0, length - len(indata['pspec'])), 'constant', constant_values=pspec_min)
+            index = 0
+        else:
+            index = np.random.randint(0, len(indata['mspec']) - length)
+
+        indata['mspec'] = indata['mspec'][index:index + length]
+        indata['pspec'] = indata['pspec'][index:index + length]
+
+    indata['pspec'] = np.clip(indata['pspec'], pspec_min, pspec_max)
+    indata['mspec'] = np.clip(indata['mspec'], mspec_min, mspec_max)
+
+    if log_spec:
+        indata['mspec'] = np.log10(indata['mspec'])
+        shift = np.log10(mspec_min)
+        scale = np.log10(mspec_max) - np.log10(mspec_min)
+        indata['mspec'] = (indata['mspec'] - shift) / scale
+
+        indata['pspec'] = np.log10(indata['pspec'])
+        shift = np.log10(pspec_min)
+        scale = np.log10(pspec_max) - np.log10(pspec_min)
+        indata['pspec'] = (indata['pspec'] - shift) / scale
+    else:
+        shift = mspec_min
+        scale = mspec_max - mspec_min
+        indata['mspec'] = (indata['mspec'] - shift) / scale
+
+        shift = pspec_min
+        scale = pspec_max - pspec_min
+        indata['pspec'] = (indata['pspec'] - shift) / scale
+
+    return indata['mspec'].astype('float32'), indata['pspec'].astype('float32')
 
 
 if __name__ == '__main__':
