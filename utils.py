f9aa8170c663a90c800acbadb435a977b6f44807
F_Tag <25476669+F-Tag@users.noreply.github.com>
2019-04-04 19:37:06 +0900

    fix

--- utils.py
+++ utils.py
@@ -3,9 +3,13 @@ from scipy.signal import check_COLA
 
 def make_stft_args(frame_period, fs, nperseg=None, window='hann', **kwargs):
     nshift = fs * frame_period // 1000
+
     if nperseg is None:
         nperseg = nshift * 2
-    dct = dict(fs=fs, window=window, nperseg=nperseg, noverlap=nperseg - nshift)
+
+    noverlap = nperseg - nshift
+
+    dct = dict(fs=fs, window=window, nperseg=nperseg, noverlap=noverlap)
     assert check_COLA(**dct)
 
     return dct
