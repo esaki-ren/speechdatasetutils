96f6978dfb2093b807eeb3cb61466c1570ff1a2d
F_Tag <25476669+F-Tag@users.noreply.github.com>
2019-11-30 08:04:44 +0900

    add normalize_rms

--- preprocess.py
+++ preprocess.py
@@ -13,13 +13,30 @@ def normalize_peak(waveform):
     return waveform * scale
 
 
-def normalize(waveform, fs, dc_removal=True, peak=True, rms=False):
+def normalize_rms(waveform, fs, gain=-11.0):
+
+    nshift = int(fs*0.05)
+    pad = waveform.size % nshift
+    wave_dc = np.pad(waveform, (0, pad), "constant")
+    framed = wave_dc.reshape(-1, nshift)
+    rms = np.sqrt((framed**2.0).mean(1))
+
+    idx = int(rms.size*0.95)
+    peak = np.sort(rms)[idx]
+    peak_db = 20 * np.log10(peak)
+    scale_db = gain - peak_db
+    scale = 10.0 ** (scale_db / 20.0)
+
+    return waveform * scale
+
+
+def normalize(waveform, fs, dc_removal=True, peak=True, rms=False, rms_gain=-11.0):
     if dc_removal:
         waveform = remove_dc(waveform, fs)
 
     if peak:
         ret = normalize_peak(waveform)
     elif rms:
-        raise NotImplementedError
+        ret = normalize_rms(waveform, fs, gain=rms_gain)
 
     return ret
