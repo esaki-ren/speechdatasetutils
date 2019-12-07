e1c84046ccfa193a552ad48be6e4965346f45474
F-Tag <25476669+F-Tag@users.noreply.github.com>
2019-12-07 18:02:04 +0900

    fix

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from scipy.signal import check_COLA
+from .preprocess import normalize
 
 
 def make_stft_args(frame_period, fs, nperseg=None, window='hann', **kwargs):
@@ -15,3 +16,15 @@ def make_stft_args(frame_period, fs, nperseg=None, window='hann', **kwargs):
 
     dct["fs"] = fs
     return dct
+
+
+def chech_gain(wave, fs, gain, cutoff):
+
+    wave = normalize(
+        wave, fs, dc_removal=True,
+        peak=False, rms=True, rms_gain=gain, cutoff=cutoff)
+
+    if np.abs(wave).max() < 0.99:
+        return gain
+    else:
+        return chech_gain(wave, fs, gain - 1, cutoff)
