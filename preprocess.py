0a393386b16628fe33eabd75f68a574bf68a8510
F-Tag <25476669+F-Tag@users.noreply.github.com>
2020-01-15 18:28:56 +0900

    fix

--- preprocess.py
+++ preprocess.py
@@ -14,7 +14,8 @@ def remove_dc2(waveform, fs, numtaps=1025, cutoff=15):
     numtaps = min(numtaps, 2**int(np.log2((len(waveform)-1)//3)) + 1)
     b = signal.firwin(numtaps, cutoff, pass_zero=False, nyq=fs / 2)
     # return signal.filtfilt(b, [1], waveform)
-    signal.filtfilt(b, [1], waveform)
+    # signal.filtfilt(b, [1], waveform)
+    signal.lfilter(b, [1], waveform)
     return waveform
 
 
