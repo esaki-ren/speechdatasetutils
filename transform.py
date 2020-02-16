1018f8953416b928b9bbf1eb1829c19eba942149
F_Tag <25476669+F-Tag@users.noreply.github.com>
2020-02-16 14:13:37 +0900

    add phase_augmentation

---
+++ transform.py
@@ -0,0 +1,14 @@
+import numpy as np
+from scipy.signal import istft, stft
+
+
+def phase_augmentation(wave):
+    _, _, Zxx = stft(wave)
+    H = np.abs(Zxx)
+    P = np.angle(Zxx)
+    w = np.random.rand() * 2 - 1
+    P += w * np.pi
+    Zxx = H * np.exp(P * 1j)
+    _, x = istft(Zxx)
+
+    return x
