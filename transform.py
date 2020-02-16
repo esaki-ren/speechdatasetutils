ac0e5b5a5be6da77fa87048aac638d9a8bdc3551
F_Tag <25476669+F-Tag@users.noreply.github.com>
2020-02-16 22:20:30 +0900

    add invert

--- transform.py
+++ transform.py
@@ -2,13 +2,17 @@ import numpy as np
 from scipy.signal import istft, stft
 
 
-def phase_augmentation(wave):
-    _, _, Zxx = stft(wave)
-    H = np.abs(Zxx)
-    P = np.angle(Zxx)
-    w = np.random.rand() * 4 - 2
-    P += w * np.pi
-    Zxx = H * np.exp(P * 1j)
-    _, x = istft(Zxx)
+def phase_augmentation(wave, invert_ratio=0.5, shift_ratio=0.5):
+    if np.random.rand() < invert_ratio:
+        wave = wave * -1.0
 
-    return x
+    if np.random.rand() < shift_ratio:
+        _, _, Zxx = stft(wave)
+        H = np.abs(Zxx)
+        P = np.angle(Zxx)
+        w = np.random.rand() * 4 - 2
+        P += w * np.pi
+        Zxx = H * np.exp(P * 1j)
+        _, wave = istft(Zxx)
+
+    return wave
