03aecd6bbcd3e92f018193b4556f2788e965cd41
F_Tag <25476669+F-Tag@users.noreply.github.com>
2020-02-16 14:20:16 +0900

    fix

--- transform.py
+++ transform.py
@@ -6,7 +6,7 @@ def phase_augmentation(wave):
     _, _, Zxx = stft(wave)
     H = np.abs(Zxx)
     P = np.angle(Zxx)
-    w = np.random.rand() * 2 - 1
+    w = np.random.rand() * 4 - 2
     P += w * np.pi
     Zxx = H * np.exp(P * 1j)
     _, x = istft(Zxx)
